b3b4e10f727af10527f40dde9e1c168bc06b42b1
Antonio <cuccu.a@gmail.com>
2020-04-27 21:26:58 -0400

    added trackanalysis route

--- server.js
+++ server.js
@@ -32,6 +32,31 @@ SpotifyApi.clientCredentialsGrant()
     console.log('authorization failed', err.message);
   });
 
+  app.get('/tracksearch', function (request, response) {
+  
+    // Search for a track!
+    SpotifyApi.searchTracks('track:what you need', {limit: 1})
+      .then((data) => {
+      
+        // Send the first (only) track object
+        response.send(data.body.tracks.items[0]);
+      
+      }, function(err) {
+        console.error(err);
+      });
+  });
+
+  app.get ('/trackanalysis/:id', (request, response) => {
+    SpotifyApi.getAudioFeaturesForTrack(request.params.id)
+      .then((data) => {
+      response.send(data)
+      }, function(err) {
+        console.error(err);});
+  });
+
+ 
+  
+
 const listener = app.listen(PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port);
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import "./styles/index.scss"
 
-window.addEventListener("DOMContentLoaded", () => {console.log("from the entry file");})
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+let trackId = "";
+$(function() {
+    
+  $.get('/tracksearch', (data) => {
+    // Display the track name
+    
+    // Display the album art
+    var img = $('<img id="albumart"/>');
+    img.attr('src', data.album.images[0].url);
+    img.appendTo('#data-container');
+
+    trackId = data.id;
+
+  });
+
+})
+
+
+
+$.get(`/trackanalysis/${trackId}`, (data) => {
+  // gets trackanalysis from searched trackId
+  $('#data-container').text(data);
+  console.log(trackId)
+    
+})
+;})
