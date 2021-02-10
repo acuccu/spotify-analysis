edb0d5b19065fe603ee4520d295ad352c7c68631
Antonio <cuccu.a@gmail.com>
2021-02-10 17:15:13 -0500

    about and modal content fixed

--- src/reset.js
+++ src/reset.js
@@ -1,11 +1,13 @@
 const dataReset = function () {
-    d3.select("#main-container").selectAll("*").remove();
+    d3.select("#main").selectAll("*").remove();
     let img = $('<div id="track-image"></div>');
-    img.appendTo("#main-container");
+    img.appendTo("#main");
     let genre = $('<div id="genre-cloud"></div>');
-    genre.appendTo("#main-container");
+    genre.appendTo("#main");
     let data = $('<div id="data-bars"></div>');
-    data.appendTo("#main-container")
+    data.appendTo("#main")
+    let dataContainer = $('<div id="data-container"></div>');
+    dataContainer.appendTo("#main")
 }
 
 export default dataReset
