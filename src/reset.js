750f4a61820342a652a250a262c1d5cb7ca5143d
Antonio <cuccu.a@gmail.com>
2021-03-02 15:36:33 -0500

    reset function fixed

--- src/reset.js
+++ src/reset.js
@@ -1,13 +1,11 @@
 const dataReset = function () {
     d3.select("#main").selectAll("*").remove();
-    let img = $('<div id="track-image"></div>');
-    img.appendTo("#main");
-    let genre = $('<div id="genre-cloud"></div>');
-    genre.appendTo("#main");
-    let data = $('<div id="data-bars"></div>');
-    data.appendTo("#main")
+    let info = $('<div id="info"><div id="data-top"><div id="track-image"></div><div id="genre-cloud"></div></div><div id="data-bars"></div></div>');
+    info.appendTo("#main");
     let dataContainer = $('<div id="data-container"></div>');
     dataContainer.appendTo("#main")
+    let labels = $('<div id="energy" >Energy</div><div id="danceability" >Danceability</div><div id="tempo" >Tempo</div><div id="valence" >Valence</div><div id="keytag" >Key</div>');
+    labels.appendTo("#main")
 }
 
 export default dataReset
